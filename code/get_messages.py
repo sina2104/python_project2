1c19789e42ede9f31e5989f46cd2d3e4598d3f53
sina2104 <79207960+sina2104@users.noreply.github.com>
2021-05-13 14:53:57 +0300

    Update get_messages.py

--- code/get_messages.py
+++ code/get_messages.py
@@ -11,29 +11,32 @@ def get_messages(message):
     '''
     Функция, с помощью которой анализируем сообщения и отправим данные
     '''
+    first_day_of_week = 1
+    last_day_of_week = 7
+    first_element = 0
     weather = requests.get(config.WEATHER)
     news = requests.get(config.NEWS)
     html = our_BS(weather.content, 'html.parser')
     html_news = our_BS(news.content, 'html.parser')
     if message.text == 'Погода сегодня':
-        text = html.select('#content .wDescription .description')[0].text
-        temp = html.select('#blockDays .today-temp')[0].text
-        time = html.select('#blockDays .today-time')[0].text
-        date = html.select('#bd1 .date')[0].text
-        month = html.select('#bd1 .month')[0].text
-        t_min = html.select('#bd1 .temperature .min')[0].text
-        t_max = html.select('#bd1 .temperature .max')[0].text
-        t_day = html.select('#bd1 .day-link')[0].text
+        text = html.select('#content .wDescription .description')[first_element].text
+        temp = html.select('#blockDays .today-temp')[first_element].text
+        time = html.select('#blockDays .today-time')[first_element].text
+        date = html.select('#bd1 .date')[first_element].text
+        month = html.select('#bd1 .month')[first_element].text
+        t_min = html.select('#bd1 .temperature .min')[first_element].text
+        t_max = html.select('#bd1 .temperature .max')[first_element].text
+        t_day = html.select('#bd1 .day-link')[first_element].text
         send_message = f"Cегодня {t_day} {date} {month}\n\n{time} {temp}\n\n{t_min}, {t_max}\n\n{text}"
 
     elif message.text == 'Погода недели':
         send_message = "погода на этой неделе:\n"
-        for i in range(1, 7):
-            month = html.select(f"#bd{i} .month")[0].text
-            t_min = html.select(f"#bd{i} .temperature .min")[0].text
-            t_max = html.select(f"#bd{i}  .temperature .max")[0].text
-            t_day = html.select(f"#bd{i} .day-link")[0].text
-            date = html.select(f"#bd{i} .date")[0].text
+        for i in range(first_day_of_week, last_day_of_week):
+            month = html.select(f"#bd{i} .month")[first_element].text
+            t_min = html.select(f"#bd{i} .temperature .min")[first_element].text
+            t_max = html.select(f"#bd{i}  .temperature .max")[first_element].text
+            t_day = html.select(f"#bd{i} .day-link")[first_element].text
+            date = html.select(f"#bd{i} .date")[first_element].text
             send_message += f"{t_day} {date} {month}\n\n{t_min},{t_max}\n\n\n"
     elif message.text == 'Новости сегодня':
         send_message = "Сегодняшние новости:"
