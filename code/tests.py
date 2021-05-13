41a767636d08ef60e4678c1ac29b85bb84b9b48c
sina2104 <79207960+sina2104@users.noreply.github.com>
2021-05-13 15:03:59 +0300

    Update tests.py

--- code/tests.py
+++ code/tests.py
@@ -7,21 +7,25 @@ class Test_get_message(unittest.TestCase):
     def setUp(self):
         self.message = aiogram.types.message.Message
         self.operate = get_messages
+        self.possible_inputs = ["1", "Unknown_text", 'Погода сегодня', 'Погода недели', 'Новости сегодня']
 
     def test_wrong_answer(self):
-        self.message.text = "1"
+        self.message.text = self.possible_inputs[0]
         self.assertEqual(self.operate(self.message), 'Я не знаю что ответить 😢')
-        self.message.text = "Unknown_text"
+        self.message.text = self.possible_inputs[1]
         self.assertEqual(self.operate(self.message), 'Я не знаю что ответить 😢')
 
     def test_get_today_weather(self):
-        self.message.text = 'Погода сегодня'
-        self.assertEqual(self.operate(self.message)[0: 7], "Cегодня")
+        message_lenth = 7
+        self.message.text = self.possible_inputs[2]
+        self.assertEqual(self.operate(self.message)[0: message_lenth], "Cегодня")
 
     def test_get_week_weather(self):
-        self.message.text = 'Погода недели'
-        self.assertEqual(self.operate(self.message)[0: 22], "погода на этой неделе:")
+        message_lenth = 22
+        self.message.text = self.possible_inputs[3]
+        self.assertEqual(self.operate(self.message)[0: message_lenth], "погода на этой неделе:")
 
     def test_get_today_news(self):
-        self.message.text = 'Новости сегодня'
-        self.assertEqual(self.operate(self.message)[0: 20], "Сегодняшние новости:")
+        message_lenth = 20
+        self.message.text = self.possible_inputs[4]
+        self.assertEqual(self.operate(self.message)[0: message_lenth], "Сегодняшние новости:")
